bbeeff53fe50613aa37116f6555721b8e3ca8779
afuzi <akiyaoka@gmail.com>
2023-10-10 02:50:24 +0900

    p5導入

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,18 @@
 "use client"
 
 // Home.tsx
-import { useState } from "react"; // React Hooksを使うためにインポート
 import './main.css'
+import Sketch from './sketch'
+
 
 const Home = () => {
-  const [count, setCount] = useState(0); // countというstateを定義し、初期値を0に設定
-  console.log(count)
-
-  const handleClick = () => { // ボタンがクリックされたときの処理を定義
-    setCount(count + 1); // countを1増やす
-    console.log('aaaa')
-  };
 
   return (
     <main>
       <div id="mainTOP">
         <div className="logoParent">
           <h1 className="logo">ポートフォリオ</h1>
+          <Sketch />
         </div>
         <div className="logoDiscription">
           説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明
