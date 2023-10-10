56f18f569ce0cb85860dc9e9e1efb6852078a900
afuzi <akiyaoka@gmail.com>
2023-10-10 23:09:59 +0900

    p5入れる

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,36 @@
 "use client"
-
-// Home.tsx
 import './main.css'
-import Sketch from './sketch'
+import React, { useState } from 'react';
+import Sketch from "react-p5";
 
 
 const Home = () => {
-  const p5element = Sketch()
-  console.log(p5element.props)
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
   return (
     <main>
       <div id="mainTOP">
         <div className="logoParent">
           <h1 className="logo">ポートフォリオ</h1>
-    
-          {p5element}
-    
         </div>
         <div className="logoDiscription">
-          説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明説明
+        <Sketch setup={setup} draw={draw} mouseClicked={handleClick} />
         </div>
-
       </div>
     </main>
   );
 };
+// 以下はp5.jsの関数です
+function setup(p5, canvasParentRef) {
+  p5.createCanvas(400, 400).parent(canvasParentRef);
+}
+
+function draw(p5) {
+  p5.background(200);
+  p5.ellipse(200, 200, 50, 50);
+}
 
 export default Home;
