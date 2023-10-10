13a4a6fd9a326002f4162ef98ef54259e26d594e
afuzi <akiyaoka@gmail.com>
2023-10-10 23:53:00 +0900

    p5のみ復活

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,10 @@
 "use client"
 import './main.css'
-// import React, { useState } from 'react';
-// import Sketch from "react-p5";
-// import p5Types from 'p5'
+import Sketch from "react-p5";
+import p5Types from 'p5'
 
 
 const Home = () => {
-  // const [count, setCount] = useState(0);
-  // function mouseClicked(p5: p5Types) {
-  //   // クリックしたときにカウントを増やす処理を書く
-  //   setCount(count + 1);
-  // }
-
   return (
     <main>
       <div id="mainTOP">
@@ -19,22 +12,21 @@ const Home = () => {
           <h1 className="logo">ポートフォリオ</h1>
         </div>
         <div className="logoDiscription">
-         {/* <Sketch setup={setup} draw={draw} /> 
-         <p>カウント: {count}</p>  */}
+         <Sketch setup={setup} draw={draw} /> 
         </div>
       </div>
     </main>
   );
 };
 // 以下はp5.jsの関数です
-// function setup(p5: p5Types, canvasParentRef: Element) {
-//   p5.createCanvas(400, 400).parent(canvasParentRef);
-// }
+function setup(p5: p5Types, canvasParentRef: Element) {
+  p5.createCanvas(400, 400).parent(canvasParentRef);
+}
 
-// function draw(p5: p5Types) {
-//   p5.background(200);
-//   p5.ellipse(200, 200, 50, 50);
-// }
+function draw(p5: p5Types) {
+  p5.background(200);
+  p5.ellipse(200, 200, 50, 50);
+}
 
 
 
