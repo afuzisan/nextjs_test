f516e97aec2c895c7a95410d28710063218b9848
afuzi <akiyaoka@gmail.com>
2023-10-10 23:50:33 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 "use client"
 import './main.css'
 // import React, { useState } from 'react';
-import Sketch from "react-p5";
-import p5Types from 'p5'
+// import Sketch from "react-p5";
+// import p5Types from 'p5'
 
 
 const Home = () => {
@@ -19,22 +19,22 @@ const Home = () => {
           <h1 className="logo">ポートフォリオ</h1>
         </div>
         <div className="logoDiscription">
-        <Sketch setup={setup} draw={draw} />
-        {/* <p>カウント: {count}</p> */}
+         {/* <Sketch setup={setup} draw={draw} /> 
+         <p>カウント: {count}</p>  */}
         </div>
       </div>
     </main>
   );
 };
 // 以下はp5.jsの関数です
-function setup(p5: p5Types, canvasParentRef: Element) {
-  p5.createCanvas(400, 400).parent(canvasParentRef);
-}
+// function setup(p5: p5Types, canvasParentRef: Element) {
+//   p5.createCanvas(400, 400).parent(canvasParentRef);
+// }
 
-function draw(p5: p5Types) {
-  p5.background(200);
-  p5.ellipse(200, 200, 50, 50);
-}
+// function draw(p5: p5Types) {
+//   p5.background(200);
+//   p5.ellipse(200, 200, 50, 50);
+// }
 
 
 
