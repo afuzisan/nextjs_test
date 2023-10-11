318672058495447020413d9fd2ae20f9be2bd8bf
afuzi <akiyaoka@gmail.com>
2023-10-11 22:32:33 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,38 @@
 import './main.css'
 import p5Types from 'p5'
 import dynamic from 'next/dynamic';
-const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
-  ssr: false,
-});
+// import { useEffect,useRef } from 'react';
+
+
+
 
 const Home = () => {
+  let x = 0;
+  let noiseVariable = 0;
+
+  const Sketch = dynamic(() => import('react-p5').then((mod) => mod.default), {
+    ssr: false,
+  });
+
+  const setup = (p5:p5Types) =>{
+    p5.createCanvas(p5.windowWidth, p5.windowHeight);
+    // frameRate(3);
+  }
+
+  const draw =(p5:p5Types)=>{
+    // random() 完全にランダム
+    let randomValue = p5.random(0, 100);
+    p5.point(x, 10000 + randomValue);
+    // console.log(randomValue);
+    // 0〜1を返す
+    let noiseValue = p5.noise(noiseVariable) * 100;
+    p5.point(x, 300 + noiseValue);
+    // noiseVariable += 0.01;
+    noiseVariable += 0.1;
+    // console.log(noiseValue);
+    x += 1;
+  }
+
 
   return (
     <main>
@@ -22,18 +49,9 @@ const Home = () => {
     </main>
   );
 };
-// 以下はp5.jsの関数です
-function setup(p5: p5Types) {
-  let canvas = p5.createCanvas(400, 400);
-  console.log(canvas.style)
-  canvas.style('z-index','-1')
-}
-
-function draw(p5: p5Types) {
-  p5.background(200);
-  p5.ellipse(200, 200, 50, 50);
-}
-
-
-
 export default Home;
+
+
+
+
+
