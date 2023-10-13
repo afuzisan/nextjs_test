914716e23de70149307999ad5a15119c834dcddd
afuzi <akiyaoka@gmail.com>
2023-10-14 01:59:45 +0900

    スクロール量を表示する処理を追加

--- app/mouseWheel.tsx
+++ app/mouseWheel.tsx
@@ -7,7 +7,10 @@ const mouseWheel = () => {
     let scrollDirection = true
     let deltaTotalBefore = 0
     let beforeScrollDirection = true
+    let first_flag = false
+    
     useEffect(()=>{
+        wheelMapCreate()
         let wheelFlag = 1
         window.addEventListener('wheel', updateScroll, { passive: false });
         function updateScroll(e:any){
@@ -15,11 +18,17 @@ const mouseWheel = () => {
             if(deltaTotal <= 600 || deltaTotal >= -600){
                 deltaTotalBefore = deltaTotal
                 deltaTotal += e.deltaY 
+                
             }
             beforeScrollDirection = scrollDirection
             deltaTotal >= deltaTotalBefore ? scrollDirection = false : scrollDirection = true
             //マウスホイールが反転した時スクロールのスコアを０に戻す
             beforeScrollDirection !== scrollDirection ? deltaTotal = 0 : deltaTotal
+            if(first_flag){
+                beforeScrollDirection !== scrollDirection ? removeEl() : null
+            }
+            first_flag = true
+            wheelNumber(deltaTotal,scrollDirection)
 
             if (deltaTotal === 600 && wheelFlag==1) {
                 wheelFlagloop('wheelFlag3','wheelFlag1')
@@ -58,7 +67,66 @@ const mouseWheel = () => {
         }
     },[])
 }
+function wheelMapCreate(){
+    let main = document.querySelector('#mainTOP')
+    let  wheelMapElement = document.createElement('div')
+    let  wheelMapElementSpan = document.createElement('span')
+    wheelMapElement.classList.add('wheelMap');
+    wheelMapElementSpan.classList.add('wheelAfter');
 
+    main.appendChild(wheelMapElement)
+    let wheelMap = document.querySelector('.wheelMap')
+    wheelMap.appendChild(wheelMapElementSpan)
+}
+
+function wheelNumber(deltaTotal:number,scrollDirection:boolean){
+    let wheelAfter = document.querySelector('.wheelAfter')
+    let wheelMap = document.querySelector('.wheelMap')
+    let wheelAfterRotate = document.querySelector('.wheelAfterRotate')
+    let wheelMapRotate = document.querySelector('.wheelMapRotate')
+    
+    if(deltaTotal > 0){
+        let deltaTotalborder = deltaTotal / 3
+        wheelAfter.style.height = deltaTotalborder+'px'
+        console.log(deltaTotal)
+    }else if(deltaTotal < 0){
+        let deltaTotalborder = deltaTotal*-1 / 3
+        wheelAfterRotate.style.height = deltaTotalborder+'px'
+        console.log(deltaTotal)
+    }
+}
+
+function removeEl(){
+    let wheelAfter = document.querySelector('.wheelAfter')
+    let wheelMap = document.querySelector('.wheelMap')
+    let wheelAfterRotate = document.querySelector('.wheelAfterRotate')
+    let wheelMapRotate = document.querySelector('.wheelMapRotate')
+    if(wheelAfter && wheelMap){
+        wheelAfter.remove()
+        wheelMap.remove()
+        let main = document.querySelector('#mainTOP')
+        let  wheelMapElementRotate = document.createElement('div')
+        let  wheelMapElementSpan = document.createElement('span')
+        wheelMapElementRotate.classList.add('wheelMapRotate');
+        wheelMapElementSpan.classList.add('wheelAfterRotate');
+        main.appendChild(wheelMapElementRotate)
+        let wheelMapRotate = document.querySelector('.wheelMapRotate')
+        wheelMapRotate.appendChild(wheelMapElementSpan)
+
+    }else if(wheelAfterRotate && wheelMapRotate){
+        wheelAfterRotate.remove()
+        wheelAfterRotate.remove()
+        let main = document.querySelector('#mainTOP')
+        let  wheelMapElement = document.createElement('div')
+        let  wheelMapElementSpan = document.createElement('span')
+        wheelMapElement.classList.add('wheelMap');
+        wheelMapElementSpan.classList.add('wheelAfter');
+        main.appendChild(wheelMapElement)
+        let wheelMap = document.querySelector('.wheelMap')
+        wheelMap.appendChild(wheelMapElementSpan)
+    }
+    
+}
 
 //flag変更とエレメント変更関数
 function wheelFlagloop(deleteFlag,flagAfterEl){
