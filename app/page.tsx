3111904c1c5cd31c9f2ab80c7869d27c131af0b1
afuzi <akiyaoka@gmail.com>
2023-10-14 10:48:36 +0900

    デプロイテスト

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ const Home = () => {
   }
 
   const draw =(p5:p5Types)=>{
-  p5.background(255);
+  p5.background(10);
   p5.fill(50);
   for (let i = 0; i < p5.windowWidth; i += size * 1) {
     for (let j = 0; j < p5.windowHeight; j += size * 1) {
@@ -35,18 +35,18 @@ const Home = () => {
 function drawRect(x:number, y:number, p5:p5Types) {
   p5.push();
   p5.translate(x, y);
-  p5.translate(p5.sin(color)*10, p5.sin(color));
+  p5.translate(p5.sin(color), p5.sin(color)*10);
   p5.shearX(1.3);
   color > 200 || color < 0 ? cFlag =! cFlag : cFlag;
-  cFlag === true ? p5.fill(p5.color(200, 204, color+=0.003))  : p5.fill(p5.color(200, 204, color-=0.003)) ;
+  cFlag === true ? p5.fill(p5.color(100, 100, color+=0.003))  : p5.fill(p5.color(100, 100, color-=0.003)) ;
   p5.ellipse(0, 0, size, size)
   p5.pop();
 }
 
   return (
     <main>
-      {/* <Sketch setup={setup} draw={draw}/> */}
-      {/* <LoadingEl /> */}
+      <Sketch setup={setup} draw={draw}/>
+      <LoadingEl />
       <div id="mainTOP">
         <div className="logoParent">
           <h1 className="logo">ポートフォリオ</h1>
