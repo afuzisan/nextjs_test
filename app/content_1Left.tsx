873be0f647889e6a6ca439dba506c68312bf1b6c
afuzi <akiyaoka@gmail.com>
2023-11-06 05:18:53 +0900

    要素に入ったらclassをつける

--- app/content_1Left.tsx
+++ app/content_1Left.tsx
@@ -1,67 +1,106 @@
 import './content_1left.css'
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 const Content_1Left = () => {
     console.log('レンダリング')
     let scrollVolume = 50
     let scrollDuration = 100
-    const [up, setUp] = useState(scrollVolume)
-    const [beforeUp, beforeSetUp] = useState(up - scrollVolume)
-    const [down, setDown] = useState(0)
+    const unit = 'vh'
+    const [up, setUp] = useState(0)
+    const [beforeUp, beforeSetUp] = useState(0)
+    const [down, setDown] = useState(-scrollVolume)
     const [beforeDown, beforeSetDown] = useState(down + scrollVolume)
     const ref = useRef<HTMLDListElement>(null)
 
     function handleClickUp(event: any) {
         event.preventDefault();
-        ref.current !== null ? ref.current.style.top = `${up}px` : null
-        console.log(up, '', beforeUp)
-
-        up >= 0 ? setUp((pre: number) => pre) : setUp((pre: number) => pre + scrollVolume)
-        up >= 0 ? beforeSetUp((pre: number) => pre = up) : beforeSetUp((pre: number) => pre = up)
+        ref.current !== null ? ref.current.style.top = `${up}${unit}` : null
+        up >= 0 ? setUp((pre: number) => pre) : setUp((pre: number) => pre + scrollVolume + 10)
+        beforeUp >= 0 ? beforeSetUp(up) : beforeSetUp((pre: number) => pre = up)
 
         setDown((pre: number) => up - scrollVolume)
         beforeSetDown((pre: number) => pre = up)
-        console.log(up + ' ', 'UP', ' ' + beforeUp, ' ' + 'beforeUP')
+
         const keyframes = [
-            { top: `${beforeUp}px` }, // 開始時の状態
-            { top: `${up}px` } // 終了時の状態
+            { top: `${beforeUp}${unit}` }, // 開始時の状態
+            { top: `${up}${unit}` } // 終了時の状態
         ];
-        // アニメーションの詳細を定義
-        const options = {
-            easing: "ease",
-            duration: scrollDuration
-        };
+        const options = animationOption()
+
         // アニメーションを実行
         ref.current !== null ? ref.current.animate(keyframes, options) : null
 
     }
 
     function handleClickDown(event: any) {
-        console.log(event)
         event.preventDefault();
-        ref.current !== null ? ref.current.style.top = `${down}px` : null
-        setDown((pre: number) => pre - scrollVolume)
+        console.log(`${down}${unit}`)
+        ref.current !== null ? ref.current.style.top = `${down}${unit}` : null
+
+        setDown((pre: number) => pre - scrollVolume - 10)
         beforeSetDown((pre: number) => pre = down)
 
         setUp((pre: number) => down + scrollVolume)
         beforeSetUp((pre: number) => pre = down)
 
-        console.log(down + ' ', 'down', ' ', beforeDown + ' ' + 'beforeDown')
         const keyframes = [
-            { top: `${beforeDown}px` }, // 開始時の状態
-            { top: `${down}px` } // 終了時の状態
+            { top: `${beforeDown}${unit}` }, // 開始時の状態
+            { top: `${down}${unit}` } // 終了時の状態
         ];
-        // アニメーションの詳細を定義
-        const options = {
-            easing: "ease",
-            duration: scrollDuration
-        };
+
+        const options = animationOption()
+
         // アニメーションを実行
         ref.current !== null ? ref.current.animate(keyframes, options) : null
+    }
+    function animationOption() {
+        // アニメーションの詳細を定義
+        return ({
+            easing: "ease",
+            duration: scrollDuration
+        })
+    }
 
+    function onmouseout() {
+        let leftEl = document.querySelector('.left')
+        console.log(leftEl)
+        if (leftEl !== null) {
+            leftEl.addEventListener('mouseout', () => {
+                // element.classList.remove('aaaa')
+                console.log('out')
+            });
+        }
+    }
 
+    function mouseOver() {
+        let addClassName = 'toolTips'
+        let daysEl = document.querySelectorAll('.days')
+        daysEl.forEach(element => {
+            element.addEventListener('mouseover', () => {
+                let daysEl = document.querySelectorAll('.days')
+                daysEl.forEach((element, index) => {
+                    if (element !== null) {
+                        element.classList.remove(addClassName)
+                    }
+                });
+                element.classList.add(addClassName)
+            })
+            let leftEl = document.querySelector('.left')
+            console.log(leftEl)
+            if (leftEl !== null) {
+                leftEl.addEventListener('mouseout', () => {
+                    element.classList.remove(addClassName)
+                    console.log('out')
+                });
+            }
+        });
 
     }
+    useEffect(() => {
+        mouseOver()
+        onmouseout()
+    }, [])
+
     return (
         <>
             <h1 className="leftH1Title">WEB制作年表</h1>
@@ -114,7 +153,6 @@ const Content_1Left = () => {
                         </dd>
                     </dl>
                 </dl>
-                {/* <Slider /> */}
             </div>
         </>
     )
