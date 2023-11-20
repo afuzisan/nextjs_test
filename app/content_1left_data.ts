59dfeb693e3ae833fe73adcd222ad7b1a2ce1323
afuzi <akiyaoka@gmail.com>
2023-11-20 16:47:01 +0900

    年表追加

--- app/content_1left_data.ts
+++ app/content_1left_data.ts
@@ -1,5 +1,22 @@
 const timelineData = [
     {
+        date: "2012年11月",
+        class: "day201211",
+        events: [
+            {
+                title: "HTMLとCSSの勉強をする",
+                description: [
+                    "自社サイトを作る為HTMLとCSSの勉強を開始",
+                
+                ],
+                link: "",
+                linkDescription: "",
+                image: "",
+                imageDescription: ""
+            }
+        ]
+    },
+    {
         date: "2012年12月",
         class: "day201212",
         events: [
@@ -7,10 +24,10 @@ const timelineData = [
                 title: "初めてのWEBサイト制作",
                 description: [
                     "自社のWEBサイトをHTMLとCSSで友達と二人で作りました。",
-                    "制作期間は3ヶ月"
+                    "制作期間1ヶ月"
                 ],
                 link: "https://www.dropbox.com/home/%E8%87%AA%E4%BD%9C%E3%82%B5%E3%82%A4%E3%83%88/CMS?di=left_nav_browse",
-                linkDescription: "DropBoxにデータが残ってます。",
+                linkDescription: "DropBoxにデータは残ってます。",
                 image: "",
                 imageDescription: ""
             }
@@ -23,8 +40,27 @@ const timelineData = [
             {
                 title: "初めてのWEBサービス制作",
                 description: [
-                    "HTMLとCSSだけで、無料ディレクトリ登録サイトのポータルサイトを作りました。",
-                    "動的な処理は出来なかったのでメールでやり取りしてました。"
+                    "HTMLとCSSを覚えたので、自社のSEO対策も兼ねて、",
+                    "無料ディレクトリ登録サイトのポータルサイトを作りました。",
+                    "制作期間3日"
+                ],
+                link: "",
+                linkDescription: "",
+                image: "",
+                imageDescription: ""
+            }
+        ]
+    },
+    {
+        date: "2013年3月",
+        class: "day201303",
+        events: [
+            {
+                title: "WordPressを知る",
+                description: [
+                    "WordPressを使うと複雑なことが簡単に出来ると知り",
+                    "興味本位で調べ始める。",
+
                 ],
                 link: "",
                 linkDescription: "",
@@ -38,11 +74,11 @@ const timelineData = [
         class: "day201304",
         events: [
             {
-                title: "WordPressを使いだす",
+                title: "WordPressを使い、初めてブログを作る",
                 description: [
-                    "WordPressのテンプレートを少しだけ改修する",
-                    "WordPressのカスタマイズをどうしてもしたかったため",
-                    "PHPを意味も分からず弄り、何度も動かなくなり絶望しながら作りました。"
+                    "WordPressのカスタマイズをするが、思うように行かず挫折する。",
+                    "WordPressのテンプレートを少しだけ改修する。",
+                    "制作期間3日"
                 ],
                 link: "",
                 linkDescription: "",
@@ -52,13 +88,34 @@ const timelineData = [
         ]
     },
     {
-        date: "2013年8月",
+        date: "2013年5月",
+        class: "day201304",
+        events: [
+            {
+                title: "JavaScriptの勉強を始める",
+                description: [
+                    "ワードプレスをきっかけにプログラミングに興味を持ち始める",
+                    "調べた所、初心者はJavaScriptから勉強すると良いと書いていた為",
+                    "JavaScriptの勉強を開始する。"
+                ],
+                link:"",
+                linkDescription: "",
+                image: "",
+                imageDescription: ""
+            }
+        ]
+    },
+    {
+        date: "2014年1月",
         class: "day201308",
         events: [
             {
                 title: "JavaScriptを使って横断検索サイトを作る",
                 description: [
-                    "一部始終という横断検索サイトを作りました。"
+                    "サイトを横断して検索する事が面倒だと感じていた為",
+                    "一部始終という横断検索サイトを作りました。",
+                    "フォームに文字を入れて、ボタンを押すとフォームに入れた文字で検索出来るというサイトでした。",
+                    "制作期間7日",
                 ],
                 link: "https://www.dropbox.com/home/%E3%82%AB%E3%83%A1%E3%83%A9%E3%80%81%E7%94%BB%E5%83%8F/img?preview=urligainomozi.png",
                 linkDescription: "",
@@ -74,11 +131,10 @@ const timelineData = [
             {
                 title: "2chまとめサイトの量産を始める",
                 description: [
-                    "当時スマホゲームの勢いがあったので、日本でリリースされるスマホゲームをピックアップして",
-                    "出来るだけ多くの2chまとめブログを作ってました。",
-                    "1日３～５サイト作り、記事の更新は全自動まとめツールでした。",
-                    "沢山作ってた理由は、何がヒットするゲームになるのか予想するのが難しかった為です。",
-                    "この時は毎月10万ぐらいずつ広告収入が増えていきました。"
+                    "リリースされるすべてのスマホゲームをピックアップし、リリース前に2cまとめサイトを事前に作る手法で",
+                    "広告収入を得る目的で、出来るだけ多くの2chまとめブログを量産してました。（1日10万PV程度）",
+                    "1日1～2サイト作る。",
+                    "制作期間1サイト2時間"
                 ],
                 link: "",
                 linkDescription: "",
@@ -92,9 +148,28 @@ const timelineData = [
         class: "day201501",
         events: [
             {
-                title: "test",
+                title: "プログラミングやサイト運営から離れる",
                 description: [
-                    "てすと"
+                    "2chまとめは、パンダアップデート2.0というGoogle検索エンジンのアップデートで",
+                    "キュレーションサイトが淘汰されました。",
+                    "私が作成していた2chまとめも全滅した為、やる気が無くなり暫くプログラミングやサイト運営から離れました。"
+                ],
+                link: "",
+                linkDescription: "",
+                image: "",
+                imageDescription: ""
+            }
+        ]
+    },
+    {
+        date: "2018年1月",
+        class: "day201801",
+        events: [
+            {
+                title: "投資系ブログを始める",
+                description: [
+                    "趣味の株式投資を題材にブログをコツコツ書き始める。",
+                    "最終的には、最大で月間20万円程稼げるブログに育ちました。"
                 ],
                 link: "",
                 linkDescription: "",
