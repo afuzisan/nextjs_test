6fbc693bf1039a7888335a3f14a55c95f629ee5e
afuzi <akiyaoka@gmail.com>
2023-11-25 08:59:21 +0900

    詳細ページのテキスト追加

--- app/content_1Right_data.ts
+++ app/content_1Right_data.ts
@@ -24,91 +24,114 @@ const items = [
         title: 'JavaScript',
         image: '/logoPacks/javascript-1.svg',
         imagePath: '/logoPacks/javascript-1.svg',
-        text: 'グリフィス天文台は、ロサンゼルスのグリフィス公園内にある天文台です。'
+        text: ['JavaScriptは、2013年から始めました。',
+        'JavaScriptは基本的なことは全般出来ます。',
+        'ライブラリで提供されてるようなアニメーションを伴った',
+        'Dom操作は出来ます。',
+        'フォーム操作やバリデーションなど',
+        'APIの操作可能です。',
+        'Canvasは勉強中で、p5.jsを使った２Dゲームの作成はした事ありますが',
+        '背景で使われるような、幾何学模様をイメージ通りに実現する事は、',
+        'まだ難しいです。',
+        'WEBGL出来ません。',
+        'React,NEXT.jsは動かすことは出来ます。深い知識はありません。',
+        'VueやNuxt.jsは出来ません。',]
     },
     { 
         name: 'ts', 
         title: 'TypeScript', 
         image: '/logoPacks/typescript.svg', 
         imagePath: '/logoPacks/typescript.svg', 
-        text: "" 
+        text:['TypeScriptは2023年から始めました。',
+            '転職を意識しだした時に、TypeScriptの重要性を感じたため',
+            '勉強を開始しました。',
+            'まだまだ慣れてませんが',
+            'chatGPTなど使い勉強しながら作成している状態です。',] 
     },
     { 
         name: 'next', 
         title: 'NEXT.js', 
         image: '/logoPacks/nextjs-13.svg', 
         imagePath: '/logoPacks/nextjs-13.svg', 
-        text: "" 
+        text:['NEXT.jsは2022年から勉強を始めました。',
+            '基本的な事はできる状態ですが、NEXT14のServerActionは',
+            'まだ触ってません。',
+            '今表示してるポートフォリオはAppRouterで作成しました。',
+        '',] 
     },
     { 
         name: 'react', 
         title: 'React', 
         image: '/logoPacks/react-1.svg', 
         imagePath: '/logoPacks/react-1.svg', 
-        text: "" 
+        text:['Reactは2022年から始めました。',
+                '動かすことは出来ますが、Hooksやキャッシュやレンダリング',
+            'ライブラリなど、勉強しなければいけない所が多々ある状態です。',
+        '何が適切なのか、わからないでいるので、情報収集しながら試行錯誤している途中です。',] 
     },
     { 
         name: 'node', 
         title: 'node.js', 
         image: '/logoPacks/nodejs-2.svg', 
         imagePath: '/logoPacks/nodejs-2.svg', 
-        text: "" 
+        text:['node.jsは2021年から始めました。',
+    '深い知識はありませんが、スクレイピングやExpressを使ったWEBサーバーやAPIサーバーの構築はできます。Prismaを使ったORマッパー、DB連携は出来ます。',] 
     },
     { 
         name: 'jquery', 
         title: 'jQuery', 
         image: '/logoPacks/jquery.svg', 
         imagePath: '/logoPacks/jquery.svg', 
-        text: "" 
+        text:[,] 
     },
     { 
         name: 'linux', 
         title: 'Linux', 
         image: '/logoPacks/linux-tux-1.svg', 
         imagePath: '/logoPacks/linux-tux-1.svg', 
-        text: "" 
+        text:[,] 
     },
     { 
         name: 'playCanvas', 
         title: 'PlayCanvas', 
         image: '/logoPacks/playcanvas-icon.png', 
         imagePath: '/logoPacks/playcanvas-icon.png', 
-        text: "" 
+        text:[,] 
     },
     { 
         name: 'wordpress', 
         title: 'WordPress', 
         image: '/logoPacks/wordpress-icon.svg', 
         imagePath: '/logoPacks/wordpress-icon.svg', 
-        text: "" 
+        text:[,] 
     },
     { 
         name: 'gas', 
         title: 'GAS', 
         image: '/logoPacks/GAS_img.png', 
         imagePath: '/logoPacks/GAS_img.png', 
-        text: "" 
+        text:[,] 
     },
     { 
         name: 'docker', 
         title: 'Docker', 
         image: '/logoPacks/docker-4.svg', 
         imagePath: '/logoPacks/docker-4.svg', 
-        text: "" 
+        text:[,] 
     },
     { 
         name: 'postgresql', 
         title: 'PostgreSQL', 
         image: '/logoPacks/postgresql.svg', 
         imagePath: '/logoPacks/postgresql.svg', 
-        text: "" 
+        text:[,] 
     },
     { 
         name: 'github', 
         title: 'GitHub', 
         image: '/logoPacks/github-icon-1.svg', 
         imagePath: '/logoPacks/github-icon-1.svg', 
-        text: "" 
+        text:[,] 
     },
 ];
 
