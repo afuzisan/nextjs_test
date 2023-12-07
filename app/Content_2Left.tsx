33a968bab601294cd4aff2cbab321a322f8aefdd
afuzi <akiyaoka@gmail.com>
2023-12-07 22:23:52 +0900

    ヘッダーのレイアウト追加

--- app/Content_2Left.tsx
+++ app/Content_2Left.tsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import Link from 'next/link'
+const headerStyle = {
+    width: '100%', 
+    height: '62px', 
+    backgroundColor: 'white',
+  };
 
+  const rightHeader = {
+    marginLeft:'auto'
+  };
+
+const headerContent = {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    height:'100%',
+    alignItems: 'center',
+    margin: 'auto',
+    maxWidth: '1120px', 
+    flexWrap: 'nowrap' as const,
+    gap:'20px', 
+    textDecoration:'none',
+    paddingLeft:'20px',
+    paddingRight:'20px',
+}
 const Content_2Left = () => {
     return (
-        <div>ポートフォリオサイト</div>
+        <div style={headerStyle}>
+        <div style={headerContent} className="headerContent">
+            <Link href="/" className="leftHeader topHeader">トップ</Link>
+            <Link href="/" className="rightHeader twitterHeader" style={rightHeader}>Twitter</Link>
+            <Link href="/" className="rightHeader githubHeader">GitHub</Link>
+            <Link href="/" className="rightHeader youtubeHeader">Youtube</Link>
+        </div>
+  </div>
     )
 }
 
