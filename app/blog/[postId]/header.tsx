cc7ddbdf6ee0f560e95d969f8aa9e3ab708dc055
afuzi <akiyaoka@gmail.com>
2023-12-08 22:15:57 +0900

    関数の切り出しリファクタリング

--- app/blog/[postId]/header.tsx
+++ app/blog/[postId]/header.tsx
@@ -1,39 +1,10 @@
 import React from 'react'
-import Link from 'next/link'
-const headerStyle = {
-    width: '100%', 
-    height: '62px', 
-    backgroundColor: 'white',
-  };
+import HeaderLink from '@/components/app/HeaderLink';
 
-  const rightHeader = {
-    marginLeft:'auto'
-  };
-
-const headerContent = {
-    display: 'flex',
-    justifyContent: 'flex-start',
-    height:'100%',
-    alignItems: 'center',
-    margin: 'auto',
-    maxWidth: '1120px', 
-    flexWrap: 'nowrap' as const,
-    gap:'20px', 
-    textDecoration:'none',
-    paddingLeft:'20px',
-    paddingRight:'20px',
-}
 
 const header = () => {
   return (
-    <div style={headerStyle}>
-        <div style={headerContent} className="headerContent">
-            <Link href="/" className="leftHeader topHeader">トップ</Link>
-            <Link href="/" className="rightHeader twitterHeader" style={rightHeader}>Twitter</Link>
-            <Link href="/" className="rightHeader githubHeader">GitHub</Link>
-            <Link href="/" className="rightHeader youtubeHeader">Youtube</Link>
-        </div>
-  </div>
+      <HeaderLink page={"blog/header"}/>
   )
 }
 
