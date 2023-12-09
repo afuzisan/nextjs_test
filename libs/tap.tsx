e99d96485c819498d13a921769a5e1b7bbc52607
afuzi <akiyaoka@gmail.com>
2023-12-10 03:52:04 +0900

    一部レスポンシブに対応

--- libs/tap.tsx
+++ libs/tap.tsx
@@ -1,12 +1,12 @@
 import { WContent_1_right, WContent_1_left, WContent_2_right, WContent_2_left } from '../app/sliderMainContent'
 import { createRoot } from 'react-dom/client';
-
+let wheelFlag = 1
+let deltaTotal = 0
 const SwipeComponent = () => {
-    let startY: number | null = null;
-    let endY: number | null = null;
+    let start: {x: number, y: number} | null = null;
+    let end: {x: number, y: number} | null = null;
     const minimumDistance = 100; // 最小幅を設定
-    let wheelFlag = 1
-    let deltaTotal = 0
+
     
     window.addEventListener('load', function(){
         // スワイプ／フリック
@@ -24,79 +24,103 @@ const SwipeComponent = () => {
     
     function logSwipeStart(event: TouchEvent) {
         // event.preventDefault();
-        startY = event.touches[0].pageY;
-        endY= event.touches[0].pageY;
+        start = {x: event.touches[0].pageX, y: event.touches[0].pageY};
+        end = {x: event.touches[0].pageX, y: event.touches[0].pageY};
     }
     
     function logSwipe(event: TouchEvent) {
             // event.preventDefault();
-            endY = event.touches[0].pageY;
+            end = {x: event.touches[0].pageX, y: event.touches[0].pageY};
     }
     
     function logSwipeEnd(event: TouchEvent) {
         // event.preventDefault();
-        if(endY !== null && startY !== null){
-            const distanceY = Math.abs(endY - startY); // 移動距離を計算
+        if(end !== null && start !== null){
+            const distanceX = Math.abs(end.x - start.x); // X軸の移動距離を計算
+            const distanceY = Math.abs(end.y - start.y); // Y軸の移動距離を計算
     
-            if(distanceY > minimumDistance) { // 最小幅以上動かした時のみ判定
-                if( 0 < (endY - startY) ) {
-                    console.log("下向き");
-                    if (wheelFlag == 1) {
-                        wheelFlagloop('wheelFlag3', 'wheelFlag1')
-                        rightANDleftContentCreate('wheelFlag1', WContent_1_right, WContent_1_left)
-                        deltaTotal = 0
-                        wheelFlag = 2
-                        // setSlider(deltaTotal,wheelFlag)
-        
-        
-                    } else if (wheelFlag == 2) {
-                        wheelFlagloop('wheelFlag1', 'wheelFlag2')
-                        rightANDleftContentCreate('wheelFlag2', WContent_2_right, WContent_2_left)
-                        deltaTotal = 0
-                        wheelFlag = 3
-                        // setSlider(deltaTotal,wheelFlag)
-        
-                    } else if (wheelFlag == 3) {
-                        wheelFlagloopEND('wheelFlag2')
-                        deltaTotal = 0
-                        wheelFlag = 1
-                        // setSlider(deltaTotal,wheelFlag)
-        
-                    } 
-                } else {
-                    // else if (wheelFlag == 1) {
-                    //     wheelFlagloop('wheelFlag1', 'wheelFlag2')
-                    //     rightANDleftContentCreate('wheelFlag2', WContent_2_right, WContent_2_left)
-                    //     deltaTotal = 0
-                    //     wheelFlag = 3
-                    //     setSlider(deltaTotal,wheelFlag)
-        
-                    // }
-                    // else if (wheelFlag == 3) {
-                    //     wheelFlagloop('wheelFlag2', 'wheelFlag1')
-                    //     rightANDleftContentCreate('wheelFlag1', WContent_1_right, WContent_1_left)
-                    //     deltaTotal = 0
-                    //     wheelFlag = 2
-                    //     setSlider(deltaTotal,wheelFlag)
-        
-                    // }
-                    // else if (wheelFlag == 2) {
-                    //     wheelFlagloopEND('wheelFlag1')
-                    //     deltaTotal = 0
-                    //     wheelFlag = 1
-                    //     setSlider(deltaTotal,wheelFlag)
-                    // }
-                     console.log("上向き");
+            if(distanceY > minimumDistance || distanceX > minimumDistance) { // 最小幅以上動かした時のみ判定
+                if( 0 > (end.y - start.y) && distanceX < minimumDistance ) {
+
+                } else if( 0 < (end.y - start.y) && distanceX < minimumDistance ) {
+
+                } else if( 0 > (end.x - start.x) && distanceY < minimumDistance) {
+                    console.log('左にスワイプ');
+                    let [newDeltaTotal, newWheelFlag] = upSwipe(wheelFlag,deltaTotal)
+                    deltaTotal = newDeltaTotal;
+                    wheelFlag = newWheelFlag;
+                } else if( 0 < (end.x - start.x) && distanceY < minimumDistance) {
+                    console.log('右にスワイプ');
+                    let [newDeltaTotal, newWheelFlag] = downSwipe(wheelFlag, deltaTotal);
+                    deltaTotal = newDeltaTotal;
+                    wheelFlag = newWheelFlag;
                 }
             }else{
-
+                
                 console.log('tap')
             }
         }
     }
+
     return(
         <></>
     )
+}
+
+function setSlider(deltaTotalNumber:number,wheelFlagNumber:number){
+    localStorage.setItem('scrollData', JSON.stringify({ wheelFlag: wheelFlagNumber, deltaTotal: deltaTotalNumber }));
+}
+function upSwipe(wheelFlag:number,deltaTotal:number){
+    if (wheelFlag == 1) {
+        wheelFlagloop('wheelFlag1', 'wheelFlag2')
+        rightANDleftContentCreate('wheelFlag2', WContent_2_right, WContent_2_left)
+        deltaTotal = 0
+        wheelFlag = 3
+
+        
+        setSlider(deltaTotal,wheelFlag)
+
+    }
+    else if (wheelFlag == 3) {
+        wheelFlagloop('wheelFlag2', 'wheelFlag1')
+        rightANDleftContentCreate('wheelFlag1', WContent_1_right, WContent_1_left)
+        deltaTotal = 0
+        wheelFlag = 2
+        setSlider(deltaTotal,wheelFlag)
+
+    }
+    else if (wheelFlag == 2) {
+        wheelFlagloopEND('wheelFlag1')
+        deltaTotal = 0
+        wheelFlag = 1
+        setSlider(deltaTotal,wheelFlag)
+    }
+    return [deltaTotal,wheelFlag]
+}
+
+function downSwipe(wheelFlag:number,deltaTotal:number): [number, number] {
+    if (wheelFlag == 1) {
+        wheelFlagloop('wheelFlag3', 'wheelFlag1')
+        rightANDleftContentCreate('wheelFlag1', WContent_1_right, WContent_1_left)
+        deltaTotal = 0
+        wheelFlag = 2
+        setSlider(deltaTotal,wheelFlag)
+
+
+    } else if (wheelFlag == 2) {
+        wheelFlagloop('wheelFlag1', 'wheelFlag2')
+        rightANDleftContentCreate('wheelFlag2', WContent_2_right, WContent_2_left)
+        deltaTotal = 0
+        wheelFlag = 3
+        setSlider(deltaTotal,wheelFlag)
+
+    } else if (wheelFlag == 3) {
+        wheelFlagloopEND('wheelFlag2')
+        deltaTotal = 0
+        wheelFlag = 1
+        setSlider(deltaTotal,wheelFlag)
+    } 
+    return [deltaTotal, wheelFlag];
 }
 
 function wheelFlagloop(deleteFlag: string, flagAfterEl: string) {
