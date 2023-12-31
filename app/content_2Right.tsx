4746d75b5c8384a87aaf101a88291b9d8d983f38
afuzi <akiyaoka@gmail.com>
2024-01-01 04:51:00 +0900

    pageNationっをローカルストレージに保存する

--- app/content_2Right.tsx
+++ app/content_2Right.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import axios, { AxiosRequestConfig, AxiosResponse, AxiosError } from 'axios'
 import './content_2Right.css'
+import localStorageOrigin from '../libs/localStorage'
 
 const SliderContentWcontent_2_right = () => {
 	const [Content, setContent] = useState<string[]>([])
@@ -18,72 +19,17 @@ const SliderContentWcontent_2_right = () => {
 	const [originalIds, setOriginalId] = useState<string[] | any[]>([])
 	const [originalDiscribe, setOriginalDiscribe] = useState<string[] | any[]>([])
 	const [originalTitle, setOriginalTitle] = useState<string[] | any[]>([])
-
-	const [currentPage, setCurrentPage] = useState<number>(1)
-
-	let ViewNumber = 3
+	const localStorageGet = localStorageOrigin('getItem')
+	const [currentPage, setCurrentPage] = useState(localStorageGet?.PageNation)
+	let ViewNumber = 4
 	useEffect(() => {
 		axios.get(`${window.location.href}/api/route`).then((res: AxiosResponse<any[]>) => {
-			const contentsAf = res.data.map(
-				(element: {
-					content: string
-					eyecatch: string
-					category: { name: string }
-					id: string
-					discribe: string
-					title: string
-				}) => element.content
-			)
-			const ImagesAf = res.data.map(
-				(element: {
-					content: string
-					eyecatch: string
-					category: { name: string }
-					id: string
-					discribe: string
-					title: string
-				}) => element.eyecatch
-			)
-			const categoryAf = res.data.map(
-				(element: {
-					content: string
-					eyecatch: string
-					category: { name: string }
-					id: string
-					discribe: string
-					title: string
-				}) => element.category.name
-			)
-			const IdAf = res.data.map(
-				(element: {
-					content: string
-					eyecatch: string
-					category: { name: string }
-					id: string
-					discribe: string
-					title: string
-				}) => element.id
-			)
-			const discribeAf = res.data.map(
-				(element: {
-					content: string
-					eyecatch: string
-					category: { name: string }
-					id: string
-					discribe: string
-					title: string
-				}) => element.discribe
-			)
-			const titleAf = res.data.map(
-				(element: {
-					content: string
-					eyecatch: string
-					category: { name: string }
-					id: string
-					discribe: string
-					title: string
-				}) => element.title
-			)
+			const contentsAf = res.data.map((element: { content: string; eyecatch: string; category: { name: string }; id: string; discribe: string; title: string }) => element.content)
+			const ImagesAf = res.data.map((element: { content: string; eyecatch: string; category: { name: string }; id: string; discribe: string; title: string }) => element.eyecatch)
+			const categoryAf = res.data.map((element: { content: string; eyecatch: string; category: { name: string }; id: string; discribe: string; title: string }) => element.category.name)
+			const IdAf = res.data.map((element: { content: string; eyecatch: string; category: { name: string }; id: string; discribe: string; title: string }) => element.id)
+			const discribeAf = res.data.map((element: { content: string; eyecatch: string; category: { name: string }; id: string; discribe: string; title: string }) => element.discribe)
+			const titleAf = res.data.map((element: { content: string; eyecatch: string; category: { name: string }; id: string; discribe: string; title: string }) => element.title)
 			setContent(contentsAf)
 			setImage(ImagesAf)
 			setcategory(categoryAf)
@@ -107,10 +53,12 @@ const SliderContentWcontent_2_right = () => {
 					<button
 						onClick={() => {
 							setCurrentPage(currentPage - 1)
+							localStorageOrigin('setItem', { PageNationNumber: currentPage - 1 })
+
 							if (currentPage <= 1 && Content.length % ViewNumber !== 0) {
 								//合計の要素数と表示する数を割り算した結果、あまりが出た場合の処理
-								console.log(Content.length % ViewNumber)
 								setCurrentPage(Math.ceil(Content.length / ViewNumber))
+								localStorageOrigin('setItem', { PageNationNumber: Math.ceil(Content.length / ViewNumber) })
 								const newContent = [
 									...Content.slice(Content.length - (Content.length % ViewNumber), Content.length),
 									...Content.slice(0, ViewNumber - (Content.length % ViewNumber)),
@@ -156,28 +104,14 @@ const SliderContentWcontent_2_right = () => {
 							} else {
 								if (currentPage === 1) {
 									setCurrentPage(Math.ceil(Content.length / ViewNumber))
+									localStorageOrigin('setItem', { PageNationNumber: Math.ceil(Content.length / ViewNumber) })
 								}
-								const newContent = [
-									...Content.slice(Content.length - ViewNumber, Content.length),
-									...Content.slice(0, Content.length - ViewNumber)
-								]
-								const newImages = [
-									...Images.slice(Images.length - ViewNumber, Images.length),
-									...Images.slice(0, Images.length - ViewNumber)
-								]
-								const newCategorys = [
-									...Categorys.slice(Categorys.length - ViewNumber, Categorys.length),
-									...Categorys.slice(0, Categorys.length - ViewNumber)
-								]
+								const newContent = [...Content.slice(Content.length - ViewNumber, Content.length), ...Content.slice(0, Content.length - ViewNumber)]
+								const newImages = [...Images.slice(Images.length - ViewNumber, Images.length), ...Images.slice(0, Images.length - ViewNumber)]
+								const newCategorys = [...Categorys.slice(Categorys.length - ViewNumber, Categorys.length), ...Categorys.slice(0, Categorys.length - ViewNumber)]
 								const newIds = [...Ids.slice(Ids.length - ViewNumber, Ids.length), ...Ids.slice(0, Ids.length - ViewNumber)]
-								const newDiscribe = [
-									...discribe.slice(discribe.length - ViewNumber, discribe.length),
-									...discribe.slice(0, discribe.length - ViewNumber)
-								]
-								const newTitle = [
-									...title.slice(title.length - ViewNumber, title.length),
-									...title.slice(0, title.length - ViewNumber)
-								]
+								const newDiscribe = [...discribe.slice(discribe.length - ViewNumber, discribe.length), ...discribe.slice(0, discribe.length - ViewNumber)]
+								const newTitle = [...title.slice(title.length - ViewNumber, title.length), ...title.slice(0, title.length - ViewNumber)]
 
 								setContent(newContent)
 								setImage(newImages)
@@ -195,6 +129,7 @@ const SliderContentWcontent_2_right = () => {
 								style={currentPage === pageNum ? { color: 'red' } : {}}
 								onClick={() => {
 									setCurrentPage(pageNum)
+									localStorageOrigin('setItem', { PageNationNumber: pageNum })
 									let originalContentDupe = [...originalContent]
 									let originalImagesDupe = [...originalImages]
 									let originalCategorysDupe = [...originalCategorys]
@@ -203,36 +138,12 @@ const SliderContentWcontent_2_right = () => {
 									let originalTitleDupe = [...originalTitle]
 									const start = (pageNum - 1) * ViewNumber
 									const end = (pageNum - 1) * ViewNumber + ViewNumber
-									const newContent = [
-										...originalContentDupe.slice(start, end),
-										...originalContentDupe.slice(end),
-										...originalContentDupe.slice(0, start)
-									]
-									const newImages = [
-										...originalImagesDupe.slice(start, end),
-										...originalImagesDupe.slice(end),
-										...originalImagesDupe.slice(0, start)
-									]
-									const newCategorys = [
-										...originalCategorysDupe.slice(start, end),
-										...originalCategorysDupe.slice(end),
-										...originalCategorysDupe.slice(0, start)
-									]
-									const newIds = [
-										...originalIdsDupe.slice(start, end),
-										...originalIdsDupe.slice(end),
-										...originalIdsDupe.slice(0, start)
-									]
-									const newDiscribe = [
-										...originalDiscribeDupe.slice(start, end),
-										...originalDiscribeDupe.slice(end),
-										...originalDiscribeDupe.slice(0, start)
-									]
-									const newTitle = [
-										...originalTitleDupe.slice(start, end),
-										...originalTitleDupe.slice(end),
-										...originalTitleDupe.slice(0, start)
-									]
+									const newContent = [...originalContentDupe.slice(start, end), ...originalContentDupe.slice(end), ...originalContentDupe.slice(0, start)]
+									const newImages = [...originalImagesDupe.slice(start, end), ...originalImagesDupe.slice(end), ...originalImagesDupe.slice(0, start)]
+									const newCategorys = [...originalCategorysDupe.slice(start, end), ...originalCategorysDupe.slice(end), ...originalCategorysDupe.slice(0, start)]
+									const newIds = [...originalIdsDupe.slice(start, end), ...originalIdsDupe.slice(end), ...originalIdsDupe.slice(0, start)]
+									const newDiscribe = [...originalDiscribeDupe.slice(start, end), ...originalDiscribeDupe.slice(end), ...originalDiscribeDupe.slice(0, start)]
+									const newTitle = [...originalTitleDupe.slice(start, end), ...originalTitleDupe.slice(end), ...originalTitleDupe.slice(0, start)]
 
 									setContent(newContent)
 									setImage(newImages)
@@ -248,8 +159,10 @@ const SliderContentWcontent_2_right = () => {
 					<button
 						onClick={() => {
 							setCurrentPage(currentPage + 1)
+							localStorageOrigin('setItem', { PageNationNumber: currentPage + 1 })
 							if (currentPage >= Math.ceil(Content.length / ViewNumber)) {
 								setCurrentPage(1)
+								localStorageOrigin('setItem', { PageNationNumber: 1 })
 								setImage(originalImages)
 								setcategory(originalCategorys)
 								setid(originalIds)
@@ -287,13 +200,7 @@ const SliderContentWcontent_2_right = () => {
 										</Link>
 										<Link href={`/blog/${Ids[index]}`}>
 											<div className='imageEl'>
-												<Image
-													src={Images[index] !== undefined ? Images[index].url : '/logoPacks/typescript.svg'}
-													alt='Image'
-													layout='fill'
-													objectFit='cover'
-													objectPosition='top right'
-												/>
+												<Image src={Images[index] !== undefined ? Images[index].url : '/logoPacks/typescript.svg'} alt='Image' layout='fill' objectFit='cover' objectPosition='top right' />
 											</div>
 										</Link>
 										<Link href={`/blog/${Ids[index]}`}>
@@ -310,13 +217,7 @@ const SliderContentWcontent_2_right = () => {
 									</Link>
 									<Link href={`/blog/${Ids[index]}`}>
 										<div className='imageEl'>
-											<Image
-												src={Images[index] !== undefined ? Images[index].url : '/logoPacks/typescript.svg'}
-												alt='Image'
-												layout='fill'
-												objectFit='cover'
-												objectPosition='top right'
-											/>
+											<Image src={Images[index] !== undefined ? Images[index].url : '/logoPacks/typescript.svg'} alt='Image' layout='fill' objectFit='cover' objectPosition='top right' />
 										</div>
 									</Link>
 									<Link href={`/blog/${Ids[index]}`}>
